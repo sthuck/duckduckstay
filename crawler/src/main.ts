016f3feb86532f0b0cda20d5fb459c190e5716d1
ben <benjamin.kramek@post.idc.ac.il>
2020-08-03 23:06:42 +0300

    Add some ES code

--- crawler/src/main.ts
+++ crawler/src/main.ts
@@ -1,1 +1,36 @@
-console.log("Hello");
+import { Client } from '@elastic/elasticsearch';
+
+const ES = process.env['ES'];
+if (!ES) {
+    throw new Error("ES env var required");
+}
+
+const client = new Client({ node: ES })
+
+async function main() {
+    // await client.index({
+    //     index: 'game-of-thrones',
+    //     body: {
+    //         character: 'Ned Stark',
+    //         quote: 'Winter is coming.'
+    //     }
+    // })
+
+    // here we are forcing an index refresh, otherwise we will not
+    // get any result in the consequent search
+    await client.indices.refresh({ index: 'game-of-thrones' })
+
+    // Let's search!
+    const { body } = await client.search({
+        index: 'game-of-thrones',
+        body: {
+            query: {
+                match: { quote: 'Ned' }
+            }
+        }
+    })
+
+    console.log(body.hits.hits)
+}
+
+main();
