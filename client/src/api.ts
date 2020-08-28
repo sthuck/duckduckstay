d2b128d022a9210da6e21d10fb50fad15c0c80fb
Aviad Hadad <aviadh@wix.com>
2020-08-28 07:24:38 +0300

    client: use acual endpoint

--- client/src/api.ts
+++ client/src/api.ts
@@ -6,16 +6,21 @@ export interface SearchResult {
   snippets: string[];
 }
 
-const chance = new Chance();
+// const chance = new Chance();
+// export const search = (term: string) => {
+//   console.log('searching for term', term);
+//   return new Promise<SearchResult[]>(resolve => {
+//     const data: SearchResult[] = new Array(chance.integer({min: 1, max: 20})).fill(0).map(() => ({
+//       url: chance.url(),
+//       title: chance.sentence({words: 4}),
+//       screenshotUrl: 'https://www.wholewhale.com/wp-content/uploads/2013/11/pp-website-thumbnail.png',
+//       snippets: new Array(3).fill(0).map(() => chance.paragraph()),
+//     }));
+//     setTimeout(() => resolve(data), 2000);
+//   });
+// };
+
 export const search = (term: string) => {
-  console.log('searching for term', term);
-  return new Promise<SearchResult[]>(resolve => {
-    const data: SearchResult[] = new Array(chance.integer({min: 1, max: 20})).fill(0).map(() => ({
-      url: chance.url(),
-      title: chance.sentence({words: 4}),
-      screenshotUrl: 'https://www.wholewhale.com/wp-content/uploads/2013/11/pp-website-thumbnail.png',
-      snippets: new Array(3).fill(0).map(() => chance.paragraph()),
-    }));
-    setTimeout(() => resolve(data), 2000);
-  });
+  fetch(`http://duckduckwebapp-env.eba-ypuyz5i6.us-east-1.elasticbeanstalk.com/api/search?search=${term}`)
+    .then(r => r.json());
 };
